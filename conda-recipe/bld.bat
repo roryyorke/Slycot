022f6670a2c348e4075a8563b8444bce41c27c51
Rory Yorke <rory.yorke@gmail.com>
2025-08-09 12:26:47 +0200

    Don't specify CMAKE_GENERATOR for Windows

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -4,7 +4,6 @@ set BLAS_ROOT=%PREFIX%
 set LAPACK_ROOT=%PREFIX%
 
 set "SKBUILD_CONFIGURE_OPTIONS=-DBLA_VENDOR=Generic"
-set "CMAKE_GENERATOR=NMake Makefiles"
 "%PYTHON%" -m pip install -v .
 
 if errorlevel 1 exit 1
