7349b7c1872aa4fb0e34cf5991d7c02425b08050
Rory Yorke <rory.yorke@gmail.com>
2025-09-06 16:28:18 +0200

    Try flang.exe instead of flang-new.exe

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,10 +1,16 @@
 :: correct FC, apparently pointed to host prefix??
-set FC=%BUILD_PREFIX%\Library\bin\flang-new.exe
+set FC=%BUILD_PREFIX%\Library\bin\flang.exe
 set BLAS_ROOT=%PREFIX%
 set LAPACK_ROOT=%PREFIX%
+
+echo DEBUG1
+dir %BUILD_PREFIX%\Library\bin
+echo DEBUG2
 
 set "SKBUILD_CONFIGURE_OPTIONS=-DBLA_VENDOR=Generic"
 set "CMAKE_GENERATOR=NMake Makefiles"
 "%PYTHON%" -m pip install -v .
+
+
 
 if errorlevel 1 exit 1
