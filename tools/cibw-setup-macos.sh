20df8a209d34d05473bf70c0c8da289845161c85
Rory Yorke <roryy@rory-lenovo>
2026-03-15 12:45:28 +0200

    are you kidding me

--- tools/cibw-setup-macos.sh
+++ tools/cibw-setup-macos.sh
@@ -6,9 +6,9 @@ brew install binutils
 
 brew list binutils
 
-brew list binutils|grep "/objcopy$"
+brew list binutils|grep "/objcopy$"|head -1
 
-objcopy_bin=$(brew list binutils|grep "/objcopy$")
+objcopy_bin=$(brew list binutils|grep "/objcopy$"|head -1)
 
 gfortran_bin=$(command -v gfortran-13)
 gcc_bin=$(command -v gcc-13)
