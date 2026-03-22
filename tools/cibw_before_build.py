c6a4639c83b3f28f0e262fe75363cc6583dc97e0
Rory Yorke <roryy@rory-lenovo>
2026-03-22 15:46:09 +0200

    Clean up cibw_before_build.py

--- tools/cibw_before_build.py
+++ tools/cibw_before_build.py
@@ -1,26 +1,39 @@
-# todo: document me
-# 1. install openblas32
-# 2. find library (libraries), copy it/them to a known location
+"""cibuildwheel setup script
+
+Installs scipy_openblas32 version specified by command-line argument
+
+Copies scipy_openblas32 to build-libs in project root.
+"""
 
 import os
 import shutil
 import subprocess
 import sys
 
+
 def install_openblas32(scipy_openblas32_version):
-    print(f'{__file__}: Installing scipy_openblas32=={scipy_openblas32_version}')
-    subprocess.run([sys.executable, '-m', 'pip', 'install', f'scipy_openblas32=={scipy_openblas32_version}'],
-                   check=True)
+    print(f"{__file__}: Installing scipy_openblas32=={scipy_openblas32_version}")
+    subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+            "--progress-bar",
+            "off",
+            f"scipy_openblas32=={scipy_openblas32_version}",
+        ],
+        check=True,
+    )
 
 
 def copy_libraries():
     import scipy_openblas32
+
     src = scipy_openblas32.get_lib_dir()
-    dst = os.path.abspath('build-libs')
-    print(f'{__file__}: Copying {src} to {dst}')
+    dst = os.path.abspath("build-libs")
+    print(f"{__file__}: Copying {src} to {dst}")
     shutil.copytree(src, dst)
-    # todo: remove when all 3 OSs work
-    print(os.listdir(dst))
 
 
 def main():
@@ -29,5 +42,5 @@ def main():
     copy_libraries()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
