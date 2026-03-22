c76b946d7d8597e5b4fb101fb692d9d98b12cd58
Rory Yorke <roryy@rory-lenovo>
2026-03-22 06:25:20 +0200

    Remove objcopy from root CMakeLists.txt, CI setup

--- tools/cibw-setup-macos.sh
+++ tools/cibw-setup-macos.sh
@@ -2,21 +2,11 @@
 
 set -ex
 
-brew install binutils
-
-brew list binutils
-
-brew list binutils|grep "/objcopy$"|head -1
-
-objcopy_bin=$(brew list binutils|grep "/objcopy$"|head -1)
-
 gfortran_bin=$(command -v gfortran-13)
 gcc_bin=$(command -v gcc-13)
 
 [ -x "${gfortran_bin}" ] || { echo "Fortran compiler ${gfortran_bin} doesn't exist or not executable"; exit 1; }
 [ -x "${gcc_bin}" ] || { echo "C compiler ${gcc_bin} doesn't exist or not executable"; exit 1; }
-[ -x "${objcopy_bin}" ] || { echo "objcopy_bin ${objcopy_bin} doesn't exist or not executable"; exit 1; }
 
 sudo ln -sf "${gfortran_bin}" /usr/local/bin/slycot-gfortran
 sudo ln -sf "${gcc_bin}" /usr/local/bin/slycot-gcc
-sudo ln -sf "${objcopy_bin}" /usr/local/bin/slycot-objcopy
