e4bc7cca08066e7595f02c1fed93431f5fd85426
Rory Yorke <roryy@rory-lenovo>
2026-03-28 08:03:24 +0200

    what's the lib filename?

--- build-tools/cibw_before_build.py
+++ build-tools/cibw_before_build.py
@@ -42,6 +42,8 @@ def copy_libraries(project_root):
 
     print(f"{__file__}: Copying {src} to {dst}")
     shutil.copytree(src, dst)
+    print(f"{dst} contents:")
+    print(os.listdir(dst))
 
 
 def main():
@@ -49,6 +51,7 @@ def main():
     project_root = sys.argv[2]
     install_openblas32(version)
     copy_libraries(project_root)
+    sys.exit(1)
 
 
 if __name__ == "__main__":
