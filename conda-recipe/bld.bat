58abdf57e987ec2b4b1db0620d92f251cfa9d8d2
Rory Yorke <roryy@rory-lenovo>
2026-03-29 11:45:12 +0200

    more cargo-culting from slycot-feedstock

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,14 +1,13 @@
 @echo on
 
-echo "--conda-recipe bld.bat"
-
 set FC=%BUILD_PREFIX%\Library\bin\flang.exe
+set BLAS_ROOT=%PREFIX%
+set LAPACK_ROOT=%PREFIX%
+
 set "SKBUILD_CONFIGURE_OPTIONS=-DBLA_VENDOR=Generic"
 set SKBUILD_BUILD_VERBOSE=true
 set "CMAKE_GENERATOR=Ninja"
 
-%PYTHON% -m pip install -vv .
+%PYTHON% -m pip install --no-build-isolation -vv .
 
 if errorlevel 1 exit 1
-
-if %ERRORLEVEL% neq 0 exit 1
