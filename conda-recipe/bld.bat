1b39a508cbacab9d924a60e08a95bff2f2f15cfd
Rory Yorke <roryy@rory-lenovo>
2026-03-29 11:24:06 +0200

    only do windows conda-build; more verbose build

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -3,9 +3,10 @@
 @rem set CC=c:/mingw64/bin/gcc.exe
 set FC=c:/mingw64/bin/gfortran.exe
 set "SKBUILD_CONFIGURE_OPTIONS=-DBLA_VENDOR=Generic"
+set SKBUILD_BUILD_VERBOSE=true
 set "CMAKE_GENERATOR=Ninja"
 
-%PYTHON% -m pip install -v .
+%PYTHON% -m pip install -vv .
 
 if errorlevel 1 exit 1
 
