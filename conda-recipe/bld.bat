6b86b488a1eac4c3c24075292daa453873234acf
Rory Yorke <roryy@rory-lenovo>
2026-04-03 07:11:29 +0200

    Debug build

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -5,6 +5,7 @@ set BLAS_ROOT=%PREFIX%
 set LAPACK_ROOT=%PREFIX%
 
 set "SKBUILD_CMAKE_ARGS=-G Ninja;-DBLA_VENDOR=Generic"
-"%PYTHON%" -m pip install -v .
+
+"%PYTHON%" -m pip install . -vv --config-settings=build.verbose=true --config-settings=logging.level=INFO
 
 if errorlevel 1 exit 1
