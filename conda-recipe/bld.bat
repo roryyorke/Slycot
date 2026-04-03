4aacc9943ff203e3f0f2aed7c24e738e07a64545
Rory Yorke <roryy@rory-lenovo>
2026-04-03 06:19:31 +0200

    Use SKBUILD_CMAKE_ARGS to choose Ninja

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -4,10 +4,7 @@ set FC=%BUILD_PREFIX%\Library\bin\flang.exe
 set BLAS_ROOT=%PREFIX%
 set LAPACK_ROOT=%PREFIX%
 
-set "SKBUILD_CONFIGURE_OPTIONS=-DBLA_VENDOR=Generic"
-set SKBUILD_BUILD_VERBOSE=true
-set "CMAKE_GENERATOR=Ninja"
-
-%PYTHON% -m pip install --no-deps --no-build-isolation -vv .
+set "SKBUILD_CMAKE_ARGS=-G Ninja;-DBLA_VENDOR=Generic"
+"%PYTHON%" -m pip install -v .
 
 if errorlevel 1 exit 1
