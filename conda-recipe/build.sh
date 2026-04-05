71b404f44f10156d404509239d0f349ddc754f75
Rory Yorke <roryy@rory-lenovo>
2026-04-05 18:10:40 +0200

    Try grayskull generated recipe

--- conda-recipe/build.sh
+++
@@ -1,7 +0,0 @@
-# ensure we are not building with old cmake files
-rm -rf _skbuild
-rm -rf _cmake_test_compile
-
-export CMAKE_GENERATOR=Ninja
-export SKBUILD_CONFIGURE_OPTIONS="-DBLA_VENDOR=Generic"
-$PYTHON -m pip install -v .
