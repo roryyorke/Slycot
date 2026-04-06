9d43bb80b3cbb55bce2ec5bd84a4e6b1dd819b38
Rory Yorke <roryy@rory-lenovo>
2026-04-06 17:13:08 +0200

    Build using slycot-feedstock scripts

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,11 +1,10 @@
-@echo on
-
+:: correct FC, apparently pointed to host prefix??
 set FC=%BUILD_PREFIX%\Library\bin\flang.exe
 set BLAS_ROOT=%PREFIX%
 set LAPACK_ROOT=%PREFIX%
 
-set "SKBUILD_CMAKE_ARGS=-G Ninja;-DBLA_VENDOR=Generic"
-
-"%PYTHON%" -m pip install . -vv --config-settings=build.verbose=true --config-settings=logging.level=INFO
+set "SKBUILD_CONFIGURE_OPTIONS=-DBLA_VENDOR=Generic"
+set "CMAKE_GENERATOR=Ninja"
+"%PYTHON%" -m pip install -v .
 
 if errorlevel 1 exit 1
