0148fa2489cecba4dd62fb627eeef3cca2b6bcce
Rory Yorke <roryy@rory-lenovo>
2026-04-19 16:55:31 +0200

    refactor macOS config + repair

--- build-tools/cibw_repair_wheel_macos.sh
+++ build-tools/cibw_repair_wheel_macos.sh
@@ -5,13 +5,15 @@
 #  Usage:
 #    bash cibw_repair_wheel_macos.sh <dest-dir> <wheel> <project-root-dir>
 #
-# See gha_cibw_setup_macos.sh, which creates slycot-gfortran used here
+# Requires environment variable SLYCOT_LIBS; this is a list of compiler runtime libraries.
 
 set -ex
 
 dest_dir=$1
 wheel=$2
 project_root=$3
+
+[ -v SLYCOT_LIBS ] || { echo "Variable SLYCOT_LIBS must be defined" 1>&2; exit 1 }
 
 # copied from Scipy
 # See https://github.com/scipy/scipy/issues/20852
@@ -21,13 +23,11 @@ install_name_tool -change @loader_path/../.dylibs/libgfortran.5.dylib @rpath/lib
 install_name_tool -change @loader_path/../.dylibs/libgcc_s.1.1.dylib @rpath/libgcc_s.1.1.dylib $lib_loc/libsci*
 install_name_tool -change @loader_path/../.dylibs/libquadmath.0.dylib @rpath/libquadmath.0.dylib $lib_loc/libsci*
 
-gfortran_libs=$(slycot-gfortran -print-search-dirs|grep "^libraries: ="|sed s/^libraries:\ =//)
-
 if [ ! -z $DYLD_LIBRARY_PATH ]; then
     echo "DYLD_LIBRARY_PATH already set: $DYLD_LIBRARY_PATH";
     exit 1;
 fi
 
-export DYLD_LIBRARY_PATH=$lib_loc:$gfortran_libs
+export DYLD_LIBRARY_PATH=$lib_loc:$SLYCOT_LIBS
 
 delocate-wheel -w $dest_dir $wheel
