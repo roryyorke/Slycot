0c9c35dbac6f631c269de73ddc8895e7d53ceee5
Rory Yorke <roryy@rory-lenovo>
2026-04-25 09:59:09 +0200

    clear CMAKE_MSVC_DEBUG_INFORMATION_FORMAT via own variable

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,20 +1,8 @@
 @echo on
 
-echo "bld.bat"
+set SKBUILD_CMAKE_ARGS="-GNinja;-DVERBOSE=ON;-DSLYCOT_CI=ON"
 
 echo SKBUILD_CMAKE_ARGS is - %SKBUILD_CMAKE_ARGS% -
-set CMAKE_MSVC_DEBUG_INFORMATION_FORMAT=""
-
-echo CMAKE_MSVC_DEBUG_INFORMATION_FORMAT is %CMAKE_MSVC_DEBUG_INFORMATION_FORMAT%
 
 %PYTHON% -m pip install --no-deps --no-build-isolation -vv .
 if %ERRORLEVEL% neq 0 exit 1
-
-rem set BLAS_ROOT=%PREFIX%
-rem set LAPACK_ROOT=%PREFIX%
-
-rem set "SKBUILD_CONFIGURE_OPTIONS=-DBLA_VENDOR=Generic"
-rem set "SKBUILD_CMAKE_ARGS=-G Ninja"
-rem "%PYTHON%" -m pip install -v .
-
-rem if errorlevel 1 exit 1
